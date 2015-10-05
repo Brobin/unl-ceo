dceb49cde92fbe790178fdcdab8b51234e558043
Brobin <tobinjbrown@gmail.com>
2015-10-04 23:02:46 -0500

    added passenger script and fixed a couple issues

--- ceo/urls.py
+++ ceo/urls.py
@@ -5,8 +5,8 @@ from cms import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/$', include('blog.urls')),
-    url(r'^events/$', include('schedule.urls')),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^events/', include('schedule.urls')),
     url(r'^$', views.home),
     url(r'^(.*)$', views.page),
 )
