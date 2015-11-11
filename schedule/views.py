a7d1cff689e054a7bf22f0141d2a25acde050994
Brobin <tobinjbrown@gmail.com>
2015-11-10 19:28:29 -0600

    don't display events that are over.

--- schedule/views.py
+++ schedule/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from schedule.models import Event
+import datetime
 
 
 def calendar(request):
-    events = Event.objects.all().order_by('start')
+    now = datetime.datetime.now()
+    events = Event.objects.filter(end__gte=now).order_by('start')
     return render(request, 'calendar.html', {'events': events})
